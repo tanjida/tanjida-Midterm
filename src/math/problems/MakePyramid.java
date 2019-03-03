7be528c6bec76f7b063b0a8045ce4b00c03d0bc2
Tanjida Haque <etuhaque@gmail.com>
2019-03-02 20:54:48 -0500

    FindMissingNumber

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,7 +13,26 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        int n=10;
+        pyramid(n);
+    }
+    public static void pyramid(int n){
+        int k = 2*n-2;
+        for (int i=0; i<n; i++){
 
-
+            for(int j= 0; j<k; j++){
+                System.out.print(" ");
+            }
+            k= k-1;
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
         }
-  }
+
+    }
+}
+
+
+
+
